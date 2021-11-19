4ba78055381c8fe87210ef1bfd0bcb78c5e8d4cc
Sunny Lee <shnlee.ti@gmail.com>
2021-11-19 16:03:25 -0300

    set gitignore

--- catalogo-video-app/src/App.tsx
+++ catalogo-video-app/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@material-ui/core';
 
 function App() {
   return (
@@ -9,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <Button/>
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
